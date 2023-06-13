62f128d38b385d671174f184050608e28f7e7bcd
gmckesey <gmckesey@gmail.com>
2023-06-13 12:42:31 -0400

    Removed unused code.

--- lib/dialogs/show_about.dart
+++ lib/dialogs/show_about.dart
@@ -12,54 +12,8 @@ showAbout(BuildContext context) async {
     package: kPackageName,
   );
 
-  Color backgroundColor = Theme.of(context).scaffoldBackgroundColor;
-  File? iconFile;
-  String? iconPath;
-  bool iconExists = false;
-
-  if (Platform.isLinux) {
-    for (String iconDirectories in iconPaths) {
-      List<String> iconElements = iconDirectories.split('/');
-      iconPath = '';
-      for (String element in iconElements) {
-        iconPath = path.join(iconPath!, element);
-      }
-
-      iconFile = File(iconPath!);
-      if (await iconFile.exists()) {
-        log.debug('File $iconPath found!');
-        iconExists = true;
-      } else {
-        log.debug('File $iconPath does not exist');
-        iconExists = false;
-        continue;
-      }
-
-      if (iconExists) {
-        log.debug('File $iconPath found!');
-        Widget image = Image.file(iconFile);
-        if (context.mounted) {
-          log.debug('context mounted');
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AboutDialog(
-                  applicationName: kProgramName,
-                  applicationVersion: kAppVersion,
-                  applicationLegalese: 'Copyright © 2023 - Gregory McKesey',
-                  applicationIcon: image,
-                );
-              });
-        } else {
-          log.warning('context is not mounted. No dialog shown');
-        }
-        break;
-      } else {
-        log.debug('File $iconPath does not exist');
-      }
-    }
-  } else if (Platform.isWindows) {
-    Widget image = Image.asset(kWindowsIconPath);
+  if (Platform.isWindows || Platform.isLinux) {
+    Widget image = Image.asset(kIconPath);
     showDialog(
         context: context,
         builder: (BuildContext context) {
